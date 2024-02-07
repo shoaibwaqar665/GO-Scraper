9f6e116e0616891b3f9d1cdb6da8698c62061aa9
Shoaib Waqar <126593142+shoaibwaqar665@users.noreply.github.com>
2024-02-08 02:48:13 +0500

    run command

--- main.go
+++ main.go
@@ -5,8 +5,12 @@ import (
 )
 
 func main() {
-	scrapeAndWriteCSV()
-	scrapLink2()
+	// scrapeAndWriteCSV()
+	// scrapLink2()
 	scrapLink3()
 	fmt.Println("Scraping and CSV writing completed successfully.")
 }
+
+
+//   go run main.go link1.go link2.go link3.go
+//  go run main.go link3.go
